8b483673f024c98ed2723864827df740f6c8ed18
Ignacio <ignacio.gibbs@gmail.com>
2024-07-17 14:37:27 -0300

    chore: Update environment variables for order microservice integration

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,8 +3,10 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+
   PRODUCT_MS_HOST: string;
   PRODUCT_MS_PORT: number;
+
   ORDER_MS_HOST: string;
   ORDER_MS_PORT: number;
 }
@@ -12,8 +14,10 @@ interface EnvVars {
 const envSchema = joi
   .object<EnvVars>({
     PORT: joi.number().required().default(3000),
+
     PRODUCT_MS_HOST: joi.string().required().default('localhost'),
     PRODUCT_MS_PORT: joi.number().required().default(3001),
+
     ORDER_MS_HOST: joi.string().required().default('localhost'),
     ORDER_MS_PORT: joi.number().required().default(3002),
   })
@@ -29,8 +33,10 @@ if (error) {
 
 export const envs = {
   port: envVars.PORT,
+
   productMsHost: envVars.PRODUCT_MS_HOST,
   productMsPort: envVars.PRODUCT_MS_PORT,
+
   orderMsHost: envVars.ORDER_MS_HOST,
   orderMsPort: envVars.ORDER_MS_PORT,
 };
